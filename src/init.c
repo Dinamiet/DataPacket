4eaf0115ba2fdd669b19fed45563657b647e0c4c
Dinamiet <iceman.sm39@gmail.com>
2024-11-11 19:56:34 +0200

    Init implementation

--- src/init.c
+++ src/init.c
@@ -1,1 +1,12 @@
 #include "datapacket.h"
+
+void DataPacket_Init(
+		DataPacket*                       dp,
+		const DataPacketMessage*          messages,
+		const DataPacket_TransferFunction read_callback,
+		const DataPacket_TransferFunction write_callback)
+{
+	dp->Messages = messages;
+	dp->Read     = read_callback;
+	dp->Write    = write_callback;
+}
