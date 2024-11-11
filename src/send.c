b490fb23a14ebecbe68403f8743dbfd1f85f7f2c
Dinamiet <iceman.sm39@gmail.com>
2024-11-11 20:10:44 +0200

    Packet sending

--- src/send.c
+++ src/send.c
@@ -1,1 +1,15 @@
 #include "datapacket.h"
+#include "packet_structure.h"
+
+#include <string.h>
+
+void DataPacket_Send(const DataPacket* dp, const uint8_t messageID, const void* data, const size_t size)
+{
+	Packet packet;
+	packet.Header.MessageID = messageID;
+	packet.Header.Length    = size;
+	packet.Header.Checksum  = 0; /** TODO: Generate checksum from data */
+	memcpy(packet.Data, data, size);
+
+	dp->Write(&packet, size + sizeof(packet.Header));
+}
